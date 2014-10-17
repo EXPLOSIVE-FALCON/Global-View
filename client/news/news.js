6fb370adb3deae070d7c07d21c17406eee3780b3
Kia Winslow <ksukop@gmail.com>
2014-10-17 11:50:55 -0700

    added GoogleNews page and controller

---
+++ client/news/news.js
@@ -0,0 +1,13 @@
+angular.module('news', [])
+
+.controller('NewsController', function ($scope, GoogleNews) {
+  $scope.data = {};
+  console.log($scope.data);
+  $scope.getNews = function(request) {
+    GoogleNews.getNews(request)
+      .then(function(data) {
+        $scope.data = data;
+      })
+  };
+  $scope.getNews({});
+});
