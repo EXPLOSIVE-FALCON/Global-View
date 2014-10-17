168e5692d200dc84aea95810caa6fa521a8e593e
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2014-10-16 20:51:01 -0700

    Instagram API is functioning properly and pulling down photos

--- apis/instagram.js
+++ apis/instagram.js
@@ -1,45 +1,45 @@
 var request = require('request');
 var querystring = require('querystring');
 var util = require('util');
-
-console.log(request);
-/*
-Instagram API
-Client ID: c46551123e2f42ef95d857356a7ff0b6
-Client Secret: cb6edb6450e4400b9984f09877824330
-*/
+var _ = require('lodash');
+var instaKeys = require('../instaKeys');
 
 /* Sample GET Request URLS for Locations and Photos, respectively
 https://api.instagram.com/v1/locations/search?lat=34&lng=-118&distance=1000
 https://api.instagram.com/v1/locations/19440498/media/recent?min_timestamp=1413061158101&max_timestamp=1413493114700
  */
 
+console.log(instaKeys);
+
 var instaSettings = {
-  headers: {
-    instaClientID: 'c46551123e2f42ef95d857356a7ff0b6',
-    instaClientKey: 'cb6edb6450e4400b9984f09877824330'
-  },
+  headers: instaKeys.keys,
   locationGET: 'https://api.instagram.com/v1/locations/search',
   photoGET: 'https://api.instagram.com/v1/locations/',
-  photGET2: '/media/recent?'
+  photoGET2: '/media/recent'
 
 };
 
-// 'access_token=',instaSettings.headers.instaClientID,'&',
-
-
 var instaLocations = function(lat,lng,perimeter) {
-  var params = ['?','lat=',lat,'&','lng=',lng,'&','distance=',perimeter].join('').trim();
-  console.log(instaSettings.locationGET + params);
-/*   console.log(params); */
-
+  var params = ['?','access_token=',instaSettings.headers.instaToken,'&','lat=',lat,'&','lng=',lng,'&','distance=',perimeter].join('').trim();
   var locationURL = util.format(instaSettings.locationGET + params);
-  console.log(locationURL);
 
   request(locationURL,function(err, res, body) {
-    if (!!err) { throw 'Erorr: ' + err; }
-    console.log('hi');
-    console.log(res);
+    if (!!err) {
+      throw 'Erorr: ' + err;
+    } else {
+      var results = JSON.parse(body);
+
+      for(var i=0;i<results.data.length;i++) {
+        // Calculate distance from lat/lng inputs in instaLocations
+        results.data[i].distance = distanceBetween(lat, results.data[i].latitude, lng, results.data[i].longitude);
+      }
+
+      results.data = _.sortBy(results.data, 'distance');
+
+      for(var j=0;j<results.data.length;j++) {
+        instaPhotos(results.data[j].id);
+      }
+    }
 
   });
 
@@ -47,8 +47,30 @@ var instaLocations = function(lat,lng,perimeter) {
 
 instaLocations(34,-118,1000);
 
-
 var instaPhotos = function(locationId) {
-
+  var locationURL = [instaSettings.photoGET,locationId,instaSettings.photoGET2,'?','access_token=',instaSettings.headers.instaToken].join('').trim();
+  request(locationURL,function(err,res,body) {
+    if(!!err) {
+      throw 'Error: ' + err;
+    } else {
+      var results = JSON.parse(body);
+      console.log(results);
+    }
+  });
 };
 
+// Calculations from: http://stackoverflow.com/questions/7672759/how-to-calculate-distance-from-lat-long-in-php
+var distanceBetween = function(lat1, lat2, lng1, lng2) {
+  var lngDifference = lng1 - lng2;
+  var latDiffernce = (Math.sin(deg2radCalc(lat1)) * Math.sin(deg2radCalc(lat2))) + (Math.cos(deg2radCalc(lat1)) * Math.cos(deg2radCalc(lat2)));
+  var distance = deg2radCalc(Math.acos(lngDifference * Math.cos(deg2radCalc(latDiffernce))));
+  var distanceMiles = distance * 60 * 1.1515;
+
+  return distance;
+
+}
+
+// Calculations from: https://github.com/kvz/phpjs/blob/master/functions/math/deg2rad.js
+var deg2radCalc = function(number) {
+  return (number / 180) * Math.PI;
+}
