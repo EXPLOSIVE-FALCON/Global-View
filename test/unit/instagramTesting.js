6cbf364937fce2cc6e072c2e333b68e2a9018c7b
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2014-10-21 13:29:38 -0700

    Progress writing tests against Instagram API

--- test/unit/instagramTesting.js
+++ test/unit/instagramTesting.js
@@ -14,5 +14,41 @@ var assert = require("assert");
 7. expect minDate and maxDate to be numbers
 8. expect minDate and maxDate to be expressed in seconds when building location URL
 9. expect dayInMilliSeconds() to equal 86,400,000
+10. expect minDate and maxDate to not be greater than 2 days apart (86400000 * 2)
 
 */
+
+// 1
+expect(typeof results.data[0].location).to.equal('object');
+expect(typeof results.data[0].location.latitude).to.equal('number');
+expect(typeof results.data[0].location.longitude).to.equal('number');
+
+// 2
+expect(typeof results.data[0].distance).to.not.equal('undefined');
+expect(typeof results.data[0].distance).to.equal('number');
+
+//3
+expect(Math.floor(distanceBetween(37.5416,37,-122.2402,-122))).to.equal(39);
+
+// 4
+expect(results.data[0].distance < results.data[1].distance).to.equal(true);
+
+// 5
+expect(typeof results.data[0].users_in_photo).to.equal('undefined');
+expect(typeof results.data[0].comments).to.equal('undefined');
+expect(typeof results.data[0].attribution).to.equal('undefined');
+
+// 8
+expect(minDate < (Date.now()/1000)).to.equal(true);
+expect(maxDate < (Date.now()/1000)).to.equal(true);
+
+
+// 9
+expect(dayInMilliSeconds()).to.equal.(86400000);
+
+// 10
+expect(maxDate - minDate < (dayInMilliSeconds() * 2)/1000).to.equal(true);
+
+
+
+
