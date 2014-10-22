2b54263d5b234ad61b9ffa45efda76101042cc3d
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2014-10-22 11:11:29 -0700

    Tests written with Mocha describe, it, expect syntax

--- test/unit/instagramTesting.js
+++ test/unit/instagramTesting.js
@@ -1,55 +1,86 @@
 var assert = require("assert");
 
-// expect(4+5).to.equal(9);
-
-// write tests, then build structure around them
-
 /*
-1. expect each of the elements in data to have a location key with an object containing at least latitude and longitude
-2. expect each of the elements in data to have a distance key
-3. expect value of data[i].distance to calculate properly
-4. expect results to be sorted in ascending order
-5. expect deleted fields to be removed
-6. expect certain fields to exist for each element
-7. expect minDate and maxDate to be numbers
-8. expect minDate and maxDate to be expressed in seconds when building location URL
-9. expect minDate and maxDate to not be greater than 2 days apart (86400000 * 2)
-10. expect minDate to always be less than or equal to maxDate
-11. expect Instagram response results to be within <distance input> of <lat input> and <lng input>
-
+1. expect inputs to instagram API call to be the right type (and exist)
+2. expect response from instagram API call to include keys with values of a certain type
+3. expect response from instagram API call to not include keys removed in trimResponse()
+4. expect min_timestamp and max_timestamp to be expressed in seconds when building location URL
+5. expect min_timestamp and max_timestamp to not be greater than 2 days apart (86400000 * 2) and min_timestamp to be less than max_timestamp
+6. expect value of data[i].distance to calculate properly, sort in ascending order and to be within <distance input> of <lat input> and <lng input>
 */
 
 // 1
-expect(typeof results.data[0].location).to.equal('object');
-expect(typeof results.data[0].location.latitude).to.equal('number');
-expect(typeof results.data[0].location.longitude).to.equal('number');
+describe('Instagram API inputs', function() {
+  it('should have 5 inputs - all of which are numbers', function() {
+    expect(typeof lat).to.equal('number');
+    expect(typeof lng).to.equal('number');
+    expect(typeof min_timestamp).to.equal('number');
+    expect(typeof max_timestamp).to.equal('number');
+    expect(typeof distance).to.equal('number');
+  });
+});
+
 
 // 2
-expect(typeof results.data[0].distance).to.not.equal('undefined');
-expect(typeof results.data[0].distance).to.equal('number');
+describe('Instagram API response', function() {
+  it('should have results of specific types', function() {
+    expect(typeof results.data[0].link).to.equal('string');
+    expect(typeof results.data[0].likes).to.equal('object');
+    expect(typeof results.data[0].likes.count).to.equal('number');
+    expect(typeof results.data[0].location).to.equal('object');
+    expect(typeof results.data[0].location.latitude).to.equal('number');
+    expect(typeof results.data[0].location.longitude).to.equal('number');
+    expect(typeof results.data[0].created_time).to.equal('string');
+    expect(typeof results.data[0].images).to.equal('object');
+    expect(typeof results.data[0].images.low_resolution).to.equal('object');
+    expect(typeof results.data[0].images.thumbnail).to.equal('object');
+    expect(typeof results.data[0].images.standard_resolution).to.equal('object');
+    expect(typeof results.data[0].distance).to.equal('number');
+  });
+});
 
-//3
-expect(Math.floor(distanceBetween(37.5416,37,-122.2402,-122))).to.equal(39);
+
+// 3
+describe('trimResponse function', function() {
+  it('should remove users_in_photo, comments and attribution', function() {
+    expect(typeof results.data[0].users_in_photo).to.equal('undefined');
+    expect(typeof results.data[0].comments).to.equal('undefined');
+    expect(typeof results.data[0].attribution).to.equal('undefined');
+  });
+});
 
 // 4
-expect(results.data[0].distance < results.data[1].distance).to.equal(true);
+describe('min_timestamp and max_timestamp inputs', function() {
+  it('should be expressed in seconds instead of milliseconds', function() {
+    expect(min_timestamp < (Date.now()/1000)).to.equal(true);
+    expect(max_timestamp < (Date.now()/1000)).to.equal(true);
+  });
+  it('should be within 2 days', function() {
+    expect(min_timestamp + (86400000 * 2) >= max_timestamp).to.equal(true);
+  });
+});
 
 // 5
-expect(typeof results.data[0].users_in_photo).to.equal('undefined');
-expect(typeof results.data[0].comments).to.equal('undefined');
-expect(typeof results.data[0].attribution).to.equal('undefined');
+describe('min_timestamp', function() {
+  it('should be less than max_timestamp', function() {
+    expect(min_timestamp <= max_timestamp).to.equal(true);
+  });
+});
 
-// 8
-expect(minDate < (Date.now()/1000)).to.equal(true);
-expect(maxDate < (Date.now()/1000)).to.equal(true);
+// 6
+describe('distance between user lat/lng inputs and lat/lng of instagram photos', function() {
+  it('should be less than user distance parameter', function() {
+    expect(results.data[0].distance <= distance).to.equal(true);
+    expect(results.data[results.data.length-1].distance <= distance).to.equal(true);
+  });
 
-// 9
-expect(minDate + (86400000 * 2) <= maxDate).to.equal(true);
+  it('should calculate correctly', function() {
+    expect(Math.floor(distanceBetween(37.5416,37,-122.2402,-122))).to.equal(39);
+  });
 
-// 10
-expect(maxDate - minDate < (dayInMilliSeconds() * 2)/1000).to.equal(true);
-
-
-
+  it('should sort correctly', function() {
+    expect(results.data[0].distance < results.data[1].distance).to.equal(true);
+  });
+});
 
 
