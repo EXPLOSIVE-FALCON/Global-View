409e1576631a6b0755fb45948a7ea96e20ea2d1d
richardavendano <avendano.richard@gmail.com>
2014-10-25 16:34:13 -0700

    auto :q

--- client/views/splash/splashMain/splash_main_move.js
+++ client/views/splash/splashMain/splash_main_move.js
@@ -2,6 +2,7 @@ var movement = {
   moving: true,
   from: [],
   left: 0,
+  movingNow: false,
   top: 0
 };
 
@@ -16,37 +17,90 @@ var initiateMovement = function() {
     };
     movement.left = Math.floor(Math.random() * dimension.width);
     movement.top = Math.floor(Math.random() * dimension.height);
-    console.log(movement.left, movement.top);
     $('html, body').animate({
       scrollLeft: movement.left,
       scrollTop: movement.top
-    }, 4500);
+    }, 7000);
   };
   shift();
-  setInterval(shift, 3500);
+  setInterval(shift, 6500);
 };
 
-var checkMovement = function(to) {
-  var from = movement.from;
-  var dist = distance(to, from);
-  movement.moving = dist > 30 ? true : false;
-  if (movement.moving && !!to.length && !!from.length) { 
-    var moveX = from[0] - to[0];
-    var moveY = from[1] - to[1];
-    moveX = Math.floor(moveX * -0.55);
-    moveY = Math.floor(moveY * -0.55);
-    window.scrollBy(moveX, moveY);
+// $(window).mousemove(function (e) {
+//   console.log('wtf');
+//     var x = $(window).innerHeight() - 50,
+//         y = $(window).scrollTop() + 50;
+//     if ($('.a_splashHome').offset().top > x) {
+//         //Down
+//         $('html, body').animate({
+//             scrollTop: 300 // adjust number of px to scroll down
+//         }, 600);
+//     }
+//     if ($('.a_splashHome').offset().top < y) {
+//         //Up
+//         $('html, body').animate({
+//             scrollTop: 0
+//         }, 600);
+//     } else {
+//         $('html, body').animate({
 
-    $('html, body').clearQueue();
-    $('html, body').stop(); 
-  }
-  moveX = typeof moveX !== 'number' ? 0 : moveX;
-  moveY = typeof moveY !== 'number' ? 0 : moveY;
-  movement.from = !!from ? to : [to[0] + moveX, to[1] + moveY];
-};
+//         });
+//     }
+// });
+// var checkMovement = function(to) {
+//   var from = movement.from;
+//   var dist = distance(to, from);
+//   if (!movement.moving) {
+//     movement.moving = dist > 25 ? true : false;
+//   }
+//   if (dist !== 0) { console.log(dist); }
+//   if (dist !== 0 && movement.moving && !!to.length && !!from.length) { 
+//     var moveX = from[0] - to[0];
+//     var moveY = from[1] - to[1];
+//     moveX = Math.floor(moveX * -0.50);
+//     moveY = Math.floor(moveY * -0.50);
+//     // window.scrollBy(moveX, moveY);
+//     smoothScroll(moveX, moveY, dist);
 
-var distance = function(first, second) {
-  var x = first[0] - second[0];
-  var y = first[1] - second[1];
-  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-};
+//     $('html, body').clearQueue();
+//     $('html, body').stop(); 
+//   }
+//   moveX = typeof moveX !== 'number' ? 0 : moveX;
+//   moveY = typeof moveY !== 'number' ? 0 : moveY;
+//   movement.from = !!from ? to : [to[0] + moveX, to[1] + moveY];
+// };
+
+// var smoothScroll = function(x, y) {
+//   var sens = 1;
+//   if (Math.abs(distance) > 1000 || movement.movingNow) { return; }
+//   movement.movingNow = true;
+//   while (Math.abs(x) > 0 || Math.abs(y) > 0) {
+//     if (x > 0) {
+//       var moveX = x > sens ? sens : x;
+//       x -= moveX;
+//     } else if (x < 0) {
+//       var moveX = x < -sens ? -sens : x;
+//       x -= moveX;
+//     } else if (x === 0) {
+//       var moveX = 0;
+//     }
+
+//     if (y > 0) {
+//       var moveY = y > sens ? sens : y;
+//       y -= moveY;
+//     } else if (y < 0) {
+//       var moveY = y < -sens ? -sens : y;
+//       y -= moveY;
+//     } else if (y === 0) {
+//       var moveY = 0;
+//     }
+//     window.scrollBy(moveX, moveY);
+//   }
+//   movement.movingNow = false;
+// };
+
+// var distance = function(first, second) {
+//   var x = first[0] - second[0];
+//   var y = first[1] - second[1];
+//   return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+// };
