8ce4f1a26297a3fcedc870416920d6e43e688720
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2014-10-24 22:51:27 -0700

    Added tests for new Instagram API calls

--- test/unit/instagramTesting.js
+++ test/unit/instagramTesting.js
@@ -7,18 +7,15 @@ var assert = require("assert");
 // 5. expect min_timestamp and max_timestamp to not be greater than 2 days apart (86400000 * 2) and min_timestamp to be less than max_timestamp
 // 6. expect value of data[i].distance to calculate properly, sort in ascending order and to be within <distance input> of <lat input> and <lng input>
 // 7. expect value of data[i].created_time to be between <min_timestamp> and <max_timestamp>
-
+// 8. expect tagMatching function to properly flag photos with hashtags that match or partially match a user's query string
 // need to call instagram API and use results
 // ?? use curl http://127.0.0.1:5000/api/instagram?lat=34&lng=-118&min_timestamp=1413747713000&max_timestamp=1413834152000&distance=1000
 
 // 1
 describe('Instagram API inputs', function() {
-  it('should have 5 inputs - all of which are numbers', function() {
-    expect(typeof lat).to.equal('number');
-    expect(typeof lng).to.equal('number');
-    expect(typeof min_timestamp).to.equal('number');
-    expect(typeof max_timestamp).to.equal('number');
-    expect(typeof distance).to.equal('number');
+  it('should have 2 inputs', function() {
+    expect(typeof allParameters).to.equal('object');
+    expect(typeof callback).to.equal('function');
   });
 });
 
@@ -75,11 +72,13 @@ describe('distance between user lat/lng inputs and lat/lng of instagram photos',
   });
 
   it('should calculate correctly', function() {
-    expect(Math.floor(distanceBetween(37.5416,37,-122.2402,-122))).to.equal(39);
+    expect(Math.floor(distance({lat: 37.4627, lng: -122.4291}, {lat: 33.5912,lng: -117.7099}))).to.equal(605);
   });
 
   it('should sort correctly', function() {
-    expect(results.data[0].distance < results.data[1].distance).to.equal(true);
+    if(results.data[0].tagMatch === results.data[1].tagMatch) {
+      expect(results.data[0].distance < results.data[1].distance).to.equal(true);
+    }
   });
 });
 
@@ -87,6 +86,19 @@ describe('distance between user lat/lng inputs and lat/lng of instagram photos',
 describe('timestamp of instagram photos', function() {
   it('should be between min_timestamp and max_timestamp', function() {
     expect(parseInt(results.data[0].created_time) <= max_timestamp && parseInt(results.data[0].created_time) >= min_timestamp).to.equal(true);
+  });
+});
+
+// 8
+describe('tagMatch attributes', function() {
+  it('should be set to 0 on hashtag matches', function() {
+    // presuming a query of 'pumpkin' with a hashtag of 'pumpkinfestival'
+    expect(applyTagFilter(results.data[0])).to.equal(0);
+  });
+
+  it('should sort correctly', function() {
+    // presuming a query of 'pumpkin' with a hashtag of 'pumpkinfestival'
+    expect(results.data[0].tagMatch < results.data[results.data.length-1].tagMatch).to.equal(true);
   });
 });
 
