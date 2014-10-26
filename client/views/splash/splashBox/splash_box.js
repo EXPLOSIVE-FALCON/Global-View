4ab0f2af464bbac0a957202404c812515f581574
richardavendano <avendano.richard@gmail.com>
2014-10-25 20:05:56 -0700

    added dashboard interaction to splash

--- client/views/splash/splashBox/splash_box.js
+++ client/views/splash/splashBox/splash_box.js
@@ -1,9 +1,20 @@
-angular.module('splashBox', [])
-.controller('BoxController', function ($scope) {
+angular.module('splashBox', ['globalMethods'])
+.controller('BoxController', function ($scope, GlobalMethods) {
   $scope.data = {
     boxes: []
   };
-  $scope.populate = function() {
-
+  $scope.populate = function(city, state, topic) {
+    console.log('hell yah');
+    var requestObj = {
+      city: city,
+      state: state,
+      query: topic,
+      date: new Date(),
+      street: "944 market st"
+    }
+    
+    GlobalMethods.getNews(requestObj);
+    GlobalMethods.getTweets(requestObj);
+    GlobalMethods.getPhotos(requestObj); 
   };
 });
