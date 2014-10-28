fb75ae6ec0e64309fea2ee7f50b847a8fc76c084
richardavendano <avendano.richard@gmail.com>
2014-10-27 18:31:58 -0700

    added topic changer

--- client/views/topics/topics.js
+++ client/views/topics/topics.js
@@ -4,10 +4,12 @@
 angular.module('topics', ['storedData', 'globalMethods'])
 .controller('TopicController', function ($scope, StoredData, GlobalMethods) {
   $scope.data = StoredData;
-  $scope.changeTopic = function(topic, lat, lng) {
+  $scope.changeTopic = function(topic, lat, lng, maxtime, mintime) {
     var requestObj = StoredData.currentCity;
     requestObj.lattitude = lat;
     requestObj.longitude = lng;
+    requestObj.max_timestamp = maxtime;
+    requestObj.min_timestamp = mintime;
     requestObj.query = topic;
     GlobalMethods.getNews(requestObj);
     GlobalMethods.getTweets(requestObj);
