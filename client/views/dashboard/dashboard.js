58ae36a81e41e4f8f0112a8e041eba239500028d
richardavendano <avendano.richard@gmail.com>
2014-10-27 17:36:27 -0700

    pulled fake request out of dashboard

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -2,25 +2,4 @@ angular.module('dashboard', ['storedData', 'globalMethods'])
 .controller('DashboardController', function ($scope, StoredData, GlobalMethods) {
   $scope.data = StoredData;
   $scope.GlobalMethods = GlobalMethods;
-  /*
-  *  these are fake requests to populate the page
-  *  photos and tweets are not working 
-  */
-  // GlobalMethods.getNews(fakeRequest);
-  // GlobalMethods.getTweets(fakeRequest);
-  // GlobalMethods.getPhotos(fakeRequest); 
-  /* 
-  *  end fake requests
-  */
-  var populate = function(city, trendingTopic) {
-
-  };  
 });
-
-var fakeRequest = {
-  city: "san francisco",
-  date: new Date(),
-  query: "baseball",
-  state: "ca",
-  street: "944 market st"
-};
